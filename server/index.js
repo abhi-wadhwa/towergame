0a3dcd592e7b7c73967615d63ee8b20e737b6157
Abhi Wadhwa <abhiwadh@usc.edu>
2026-01-13 04:24:43 -0800

    none

--- server/index.js
+++ server/index.js
@@ -7,9 +7,15 @@ const app = express();
 app.use(cors());
 
 const server = createServer(app);
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://127.0.0.1:5173',
+  'https://towergame-ten.vercel.app',   // <-- your prod URL
+];
+
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: allowedOrigins,
     methods: ['GET', 'POST'],
   },
 });
